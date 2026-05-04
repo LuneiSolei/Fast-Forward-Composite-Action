98a1cf46dbda524fa7a4c8daebaa523d75998a6d
Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-04 09:04:21 -0500

    Update fast-forward.sh
    Signed-off-by: Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>

--- scripts/fast-forward.sh
+++ scripts/fast-forward.sh
@@ -2,46 +2,6 @@
 
 set -e
 
-# Ensure DEBUG value is valid
-case "${DEBUG:-true}" in
-    false) DEBUG=0;;
-    true) DEBUG=1;;
-    *)
-        echo "Warning: Invalid value '$DEBUG' for DEBUG. Using default value 'true'." >&2;
-        DEBUG=1
-        ;;
-esac
-
-# Ensure COMMENT value is valid
-case "${COMMENT:-always}" in
-    never) COMMENT=never;;
-    always) COMMENT=always;;
-    on-error) COMMENT=on-error;;
-    *)
-        echo "Warning: Invalid value '$COMMENT' for COMMENT. Using default value 'always'" >&2;
-        COMMENT=on-error
-        ;;
-esac
-
-# Ensure we're running via GitHub Actions
-if [ -z "${GITHUB_EVENT_PATH:-}" ]
-then
-    echo "GITHUB_EVENT_PATH environment variable must be set."
-fi
-
-# Do some logging if DEBUG is enabled
-if [ "${DEBUG}" -eq 1 ]
-then
-    {
-        echo env
-        env
-        echo GITHUB_ENV: ${GITHUB_ENV}
-        cat ${GITHUB_ENV}
-        echo GITHUB_EVENT_PATH: ${GITHUB_EVENT_PATH}
-        cat ${GITHUB_EVENT_PATH}
-    } >&2
-fi
-
 # Returns the value of the first key in the github.event data structure that is not null.
 function github_event {
     while [ "$#" -gt 0 ]
