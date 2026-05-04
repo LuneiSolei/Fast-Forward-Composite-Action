5504ba7d62fcfc235dae3bcc56927664201cee0d
Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-04 16:53:15 -0500

    Update prechecks.sh
    Signed-off-by: Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>

--- scripts/prechecks.sh
+++ scripts/prechecks.sh
@@ -2,42 +2,56 @@
 
 set -e
 
-# Ensure DEBUG value is valid
-case "${DEBUG:-true}" in
-    false) DEBUG=0;;
-    true) DEBUG=1;;
+# Ensure COMMENT value is valid
+case "${COMMENT}" in
+    always|on-error|never) ;;
     *)
-        echo "Warning: Invalid value '$DEBUG' for DEBUG. Using default value 'true'." >&2;
-        DEBUG=1
+        echo "::error::Invalid value '${COMMENT}' for COMMENT." >&2
+        exit 1
         ;;
 esac
 
-# Ensure COMMENT value is valid
-case "${COMMENT:-always}" in
-    never) COMMENT=never;;
-    always) COMMENT=always;;
-    on-error) COMMENT=on-error;;
+# Ensure DEBUG value is valid
+case "${DEBUG}" in
+    true|false) ;;
+    *) echo "::error::Invalid value '${DEBUG}' for DEBUG." >&2
+    exit 1
+    ;;
+esac
+
+# Ensure GITHUB_TOKEN is set
+if [[ -z "${GITHUB_TOKEN}" ]]
+then
+    echo "::error::Invalid value '${GITHUB_TOKEN}' for GITHUB_TOKEN." >&2
+    exit 1
+fi
+
+# Ensure AUTO_MERGE is valid
+case "${AUTO_MERGE}" in
+    true|false) ;;
     *)
-        echo "Warning: Invalid value '$COMMENT' for COMMENT. Using default value 'always'" >&2;
-        COMMENT=on-error
+        echo "::error::Invalid value '${AUTO_MERGE}' for AUTO_MERGE." >&2
+        exit 1
         ;;
 esac
+
 
 # Ensure we're running via GitHub Actions
-if [ -z "${GITHUB_EVENT_PATH:-}" ]
+if [[ -z "${GITHUB_EVENT_PATH}" ]]
 then
-    echo "GITHUB_EVENT_PATH environment variable must be set."
+    echo "::error::GITHUB_EVENT_PATH environment variable must be set."
+    exit 1
 fi
 
 # Do some logging if DEBUG is enabled
-if [ "${DEBUG}" -eq 1 ]
+if [[ "${DEBUG}" == "true" ]]
 then
     {
-        echo env
+        echo "env"
         env
-        echo GITHUB_ENV: ${GITHUB_ENV}
-        cat ${GITHUB_ENV}
-        echo GITHUB_EVENT_PATH: ${GITHUB_EVENT_PATH}
-        cat ${GITHUB_EVENT_PATH}
+        echo "GITHUB_ENV: ${GITHUB_ENV}"
+        cat "${GITHUB_ENV}"
+        echo "GITHUB_EVENT_PATH: ${GITHUB_EVENT_PATH}"
+        cat "${GITHUB_EVENT_PATH}"
     } >&2
 fi
