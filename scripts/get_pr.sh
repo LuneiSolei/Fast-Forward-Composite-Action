4586b7f9b4988c846a0ce49a22b158fde86b3d4f
Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-04 17:24:52 -0500

    Create get_pr.sh
    Signed-off-by: Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>

---
+++ scripts/get_pr.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+
+set -e
+
+# Generate comment header. @&ZeroWidthSpace to prevent @-mention notifications
+echo "Triggered from $(./github_event.sh .comment.html_url .pull_request.html_url) by [@&ZeroWidthSpace;${GITHUB_ACTOR}](https://github.com/$GITHUB_ACTOR)."
+echo
+
+# Get the base branch name
+BASE_REF=$(./github_pull_request.sh .base.ref)
+
+# Get the base branch SHA. 
+# If .git doesn't exist or branch is null, returns an empty string.
+BASE_SHA="$(test -d .git && git rev-parse origin/$BASE_REF 2>/dev/null || true)"
+
+# Could not resolve the SHA, clone the repository
+if [[ -z "${BASE_SHA}" ]]
+then
+  CLONE_URL=$(./github_pull_request.sh .base.repo.clone_url)
+
+  # Configure git credential helper to store credentials
+  git config --global credential.helper store
+  {
+    echo "url=${CLONE_URL}"
+    echo "username=${GITHUB_ACTOR}"
+    echo "password=${GITHUB_TOKEN}"
+  } | git credential approve
+
+  # Inject credentials into clone URL
+  CLONE_URL="${CLONE_URL%://*}://${GITHUB_ACTOR}@${CLONE_URL#*://}"
+
+  # Clone only the base branch
+  git clone --quiet --single-branch --branch "${BASE_REF}" "${CLONE_URL}" .
+
+  # Get the base branch SHA
+  BASE_SHA="$(git rev-parse origin/${BASE_REF} 2>/dev/null)"
+fi
+
+# Get the head branch ref and commit SHA
+HEAD_REF=$(./github_pull_request.sh .head.ref)
+HEAD_SHA=$(./github_pull_request.sh .head.sha)
+
+# Check if we have the PR commit already.
+# Required if we're in a fork.
+if ! git cat-file -e "${HEAD_SHA}" 2>/dev/null
+then
+  # If PR is from a fork, fetch the fork's contents too
+  CLONE_URL=$(./github_pull_request.sh .head.repo.clone_url)
+
+  # Set up credentials again, but this time for the fork
+  git config --global credential.helper store
+  {
+    echo "url=${CLONE_URL}"
+    echo "username=${GITHUB_ACTOR}"
+    echo "password=${GITHUB_TOKEN}"
+  } | git credential approve
+
+  # Inject credentials into clone URL
+  CLONE_URL="${CLONE_URL%://*}://${GITHUB_ACTOR}@${CLONE_URL#*://}"
+
+  # Fetch the PR commit
+  git fetch --quiet "${CLONE_URL}" "${HEAD_SHA}"
+fi
