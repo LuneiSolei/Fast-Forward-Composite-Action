3d52ece13cb77736f3594276fbd3fc9d50a7b9ce
Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-04 16:57:00 -0500

    Update fast-forward.sh
    Signed-off-by: Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>

--- scripts/fast-forward.sh
+++ scripts/fast-forward.sh
@@ -4,22 +4,7 @@ set -e
 
 ./prechecks.sh
 
-# Returns the value of the first key in the github.event data structure that is not null.
-function github_event {
-    while [ "$#" -gt 0 ]
-    do
-        VALUE=$(jq -r "${1}" <"${GITHUB_EVENT_PATH}")
-        if [ -n "${VALUE}" ] && [ "${VALUE}" != "null" ]
-        then
-            echo "${VALUE}"
-            return 0
-        fi
 
-        shift
-    done
-
-    return 1
-}
 
 # Create a temp file for storing metadata
 GITHUB_PR=$(mktemp)
