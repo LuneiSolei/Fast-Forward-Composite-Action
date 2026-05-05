ba4a37dacf19af566bca4f2ba30288f420fe18ec
Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-05 17:31:04 -0500

    Update fast-forward.sh
    Signed-off-by: Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>

--- scripts/fast-forward.sh
+++ scripts/fast-forward.sh
@@ -5,56 +5,6 @@ set -e
 EXIT_CODE=$(mktemp)
 echo 1 >${EXIT_CODE}
 
-# Create a temporary file to capture output for later use as a comment.
-# Everything inside the braces is captured to both the GitHub step summary
-# and the LOG file via "tee" at the end.
-LOG=$(mktemp)
-{
-  # Create a local branch reference for the PR
-  git branch -f "pull_request/${HEAD_REF}" "${HEAD_SHA}"
-
-  # Display dynamic message depending on if "merge" is enabled
-  case "${AUTO_MERGE}" in
-    "true")
-      echo -n "Trying to "
-      ;;
-    "false")
-      echo -n "Checking if we can "
-      ;;
-  esac
-  
-  echo "fast forward \`${BASE_REF}\` (${BASE_SHA}) to \`${HEAD_REF}\` (${HEAD_SHA})."
-  echo
-
-  # Show the current state of the target branch
-  echo "Target branch (\`${BASE_REF}\`):"
-  echo
-  echo '```shell'
-  git log --decorate=short -n 1 "${BASE_SHA}"
-  echo '```'
-  echo
-
-  # Show the current state of the head branch
-  echo "Pull request (\`${HEAD_REF}\`):"
-  echo
-  echo '```shell'
-  git log --decorate=short -n 1 "${HEAD_SHA}"
-  echo '```'
-
-  # Check if the base branch is a direct ancestor of the head branch
-  if ! git merge-base --is-ancestor "${BASE_REF}" "${HEAD_SHA}"
-  then
-    ${GITHUB_ACTION_PATH}/scripts/show-not-possible.sh
-
-  elif [[ "${AUTO_MERGE}" == "true" ]]
-  then
-    ${GITHUB_ACTION_PATH}/scripts/show-is-possible.sh
-  fi
-} 2>&1 | tee -a "${GITHUB_STEP_SUMMARY}" "${LOG}"
-
-COMMENT_CONTENT=$(mktemp)
-jq -n --rawfile log "$LOG" '{ "body": $log }' >"${COMMENT_CONTENT}"
-
 # Export the comment as a GitHub Actions output variable.
 {
   echo "comment<<EOF_${COMMENT_CONTENT}"
