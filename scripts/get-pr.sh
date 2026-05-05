42a4e0cd7ad7693e852d5bbbece6b35ea70dc435
Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-05 13:35:54 -0500

    Update get-pr.sh
    Signed-off-by: Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>

--- scripts/get-pr.sh
+++ scripts/get-pr.sh
@@ -12,8 +12,6 @@ printf '%s\n' "Triggered from $(${GITHUB_ACTION_PATH}/scripts/github-event.sh .c
 .pull_request.html_url) by [@&ZeroWidthSpace;${GITHUB_ACTOR}](https://github.com/$GITHUB_ACTOR)." \
 >> "${GITHUB_STEP_SUMMARY}"
 
-echo "::debug::Token: ${GITHUB_TOKEN}"
-
 # Get the base branch name
 BASE_REF=$(${GITHUB_ACTION_PATH}/scripts/github-pull-request.sh .base.ref)
 printf 'base-ref=%s\n' "${BASE_REF}" >> "${GITHUB_OUTPUT}"
