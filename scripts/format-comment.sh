55260aebc811e9a15559b6b407459a21111184d5
Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-05 20:56:00 -0500

    Update format-comment.sh
    Signed-off-by: Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>

--- scripts/format-comment.sh
+++ scripts/format-comment.sh
@@ -8,51 +8,44 @@ COMMENT_FILE=$(mktemp)
   # Display dynamic message depending on if "auto merge" is enabled
   case "${AUTO_MERGE}" in
     "true")
-      echo -n "Trying to "
+      printf 'Trying to '
       ;;
     "false")
-      echo -n "Checking if we can "
+      printf 'Checking if we can '
       ;;
   esac
   
-  echo "fast forward \`${BASE_REF}\` (${BASE_SHA}) to \`${HEAD_REF}\` (${HEAD_SHA})."
-  echo
+  printf 'fast forward `%s` (%s) to `%s` (%s).\n\n' "${BASE_REF}" "${BASE_SHA}" "${HEAD_REF}" "${HEAD_SHA}"
   
   # Show the current state of the target branch
-  echo "Target branch (\`${BASE_REF}\`):"
-  echo
-  echo '```shell'
+  printf 'Target branch (`%s`):\n\n' "${BASE_REF}"
+  printf '```shell\n'
   git log --decorate=short -n 1 "${BASE_SHA}"
-  echo '```'
-  echo
+  printf '```\n\n'
   
   # Show the current state of the head branch
-  echo "Pull request (\`${HEAD_REF}\`):"
-  echo
-  echo '```shell'
+  printf 'Pull request (`%s`):\n\n' "${HEAD_REF}"
+  printf '```shell\n'
   git log --decorate=short -n 1 "${HEAD_SHA}"
-  echo '```'
-} 2>&1 | tee -a "${GITHUB_STEP_SUMMARY}" "${LOG}"
+  printf '```\n'
+} 2>&1 | tee -a "${GITHUB_STEP_SUMMARY}"
 
 if [[ "${IS_POSSIBLE}" == "false" ]]
 then
   # Capture comment output for later step
-  echo -n "Can't fast forward \`${BASE_REF}\` (${BASE_SHA}) to" >> "${COMMENT_FILE}"
-  echo -n " \`${HEAD_REF}\` (${HEAD_SHA})." >> "${COMMENT_FILE}"
-  echo -n " \`${BASE_REF}\` (${BASE_SHA}) is not a direct ancestor of" >> "${COMMENT_FILE}"
-  echo -n " \`${HEAD_REF}\` (${HEAD_SHA})." >> "${COMMENT_FILE}"
+  printf 'Can\'t fast forward `%s` (%s) to `%s` (%s). ' "${BASE_REF}" "${BASE_SHA}" "${HEAD_REF}" "${HEAD_SHA}" >> "${COMMENT_FILE}"
+  printf '`%s` (%s) is not a direct ancestor of `%s` (%s). ' "${BASE_REF}" "${BASE_SHA}" "${HEAD_REF}" "${HEAD_SHA}" >> "${COMMENT_FILE}"
 
   # Get where the branches diverged
   MERGE_BASE=$(git merge-base "${BASE_SHA}" "${HEAD_SHA}" || true)
   if [[ -z "${MERGE_BASE}" ]]
   then
     # No common ancestor
-    echo " Branches don't appear to have a common ancestor." >> "${COMMENT_FILE}"
+    printf 'Branches don\'t appear to have a common ancestor.' >> "${COMMENT_FILE}"
   else
     # Found divergence point
-    echo " Branches appear to have diverged at ${MERGE_BASE}:" >> "${COMMENT_FILE}"
-    echo "" >> "${COMMENT_FILE}"
-    echo '```shell' >> "${COMMENT_FILE}"
+    printf 'Branches appear to have diverged at %s\n\n' "${MERGE_BASE}" >> "${COMMENT_FILE}"
+    printf '```shell\n' >> "${COMMENT_FILE}"
   
     # If ${MERGE_BASE} is a root (has no parent), then it is not a valid reference
     if git cat-file -t "${MERGE_BASE}^" &>/dev/null
@@ -67,43 +60,48 @@ then
     # Display graph of diverged branches
     git log --pretty=oneline --graph ${EXCLUDE} "${BASE_SHA}" "${HEAD_SHA}" >> "${COMMENT_FILE}"
       
-    echo "" >> "${COMMENT_FILE}"
+    printf '\n' >> "${COMMENT_FILE}"
   
     # Display details of the divergence point
     git log --decorate=short -n 1 "${MERGE_BASE}" >> "${COMMENT_FILE}"
-    echo '```' >> "${COMMENT_FILE}"
+    printf '```\n' >> "${COMMENT_FILE}"
   fi
-  echo "" >> "${COMMENT_FILE}"
-  echo "Rebase locally, and then force push to \`${HEAD_REF}\`." >> "${COMMENT_FILE}"
+  printf '\n' >> "${COMMENT_FILE}"
+  printf 'Rebase locally and then force push to `%s`.\n' "${HEAD_REF}" >> "${COMMENT_FILE}"
 elif [[ "${AUTO_MERGE}" == "false" ]]
 then
   # Fast-forwarding is possible, but "auto merge" is disabled
-
+  printf 'It is possible to fast forward `%s` (%s) to `%s` (%s), ' "${BASE_REF}" "${BASE_SHA}" "${HEAD_REF}" "${HEAD_SHA}" >> "${COMMENT_FILE}"
+  printf 'but \'auto_merge\' has been disabled.\N' >> "${COMMENT_FILE}"
+  
+  echo "EXIT_CODE=0" >> ${GITHUB_ENV}
 elif [[ "${HAS_PERMS}" == "false" ]]
 then
   # User does not have permission to push
-  echo -n "Sorry @$(${GITHUB_ACTION_PATH}/scripts/github-event.sh .sender.login),"
-  echo -n " it is possible to fast forward \`${BASE_REF}\` (${BASE_SHA})"
-  echo -n " to \`${HEAD_REF}\` (${HEAD_SHA}), but you don't appear to have"
-  echo " permission to push to this repository."
+  printf 'Sorry, @%s' "$(${GITHUB_ACTION_PATH}/scripts/github-event.sh .sender.login), " >> "${COMMENT_FILE}"
+  printf 'it is possible to fast forward `%s` (%s) to ' "${BASE_REF}" "${BASE_SHA}"
+  printf '`%s` (%s), but you don\'t appear to have ' "${HEAD_REF}" "${HEAD_SHA}" >> "${COMMENT_FILE}"
+  printf 'permission to push to this repository.\N' >> "${COMMENT_FILE}"
+  
+  echo "EXIT_CODE=0" >> ${GITHUB_ENV}
 elif [[ -n "${MERGE_COMMAND}" ]]
 then
-  echo -n "It is possible to fast forward \`${BASE_REF}\` (${BASE_SHA})"
-  echo -n " to \`${HEAD_REF}\` (${HEAD_SHA}). If you have write access to the"
-  echo -n " target repository, you can add a comment with"
-  echo -n " \`/fast-forward\` to fast forward \`${BASE_REF}\` to"
-  echo " \`${HEAD_REF}\`."
+  printf 'It is possible to fast forward `%s` (%s) ' "${BASE_REF}" "${BASE_SHA}" >> "${COMMENT_FILE}"
+  printf 'to `%s` (%s). If you have write access to the ' "${HEAD_REF}" "${HEAD_SHA}" >> "${COMMENT_FILE}"
+  printf 'target repository, you can add the comment `/fastforward` to fast forward ' >> "${COMMENT_FILE}" 
+  printf '`%s` to `%s`.\n' "${BASE_REF}" "${HEAD_REF}" >> "${COMMENT_FILE}"
+  
   echo "EXIT_CODE=0" >> ${GITHUB_ENV}
 fi
 
 COMMENT_CONTENT=$(mktemp)
 # Write to GitHub output
 {
-  echo "comment-body<<EOF"
+  printf 'comment-body<<EOF'
   cat "${COMMENT_FILE}"
-  echo ""
+  printf '\n'
   cat "${PUSH_LOG}"
-  echo "EOF"
+  printf '\n'
 } >> "${COMMENT_CONTENT}"
 
 # Determine whether to post the comment based on the setting
@@ -113,7 +111,7 @@ then
   COMMENTS_URL="$(${GITHUB_ACTION_PATH}/scripts/github-pull-request.sh .comments_url)"
   if [ -n "${COMMENTS_URL}" ]
   then
-    echo "Posting comment to ${COMMENTS_URL}."
+    printf 'Posting comment to %s.' "${COMMENTS_URL}"
     curl --silent --show-error --location --globoff \
       -X POST \
       -H "Accept: application/vnd.github+json" \
@@ -122,7 +120,7 @@ then
       "${COMMENTS_URL}" \
       -d "@${COMMENT_CONTENT}"
   else
-      echo "::error::Can't post a comment: github.event.pull_request.comments_url is not set." | tee -a ${GITHUB_STEP_SUMMARY}
+      printf '::error::Can\'t post a comment: github.event.pull_request.comments_url is not set.' | tee -a ${GITHUB_STEP_SUMMARY}
   fi
 else
     echo "Not posting comment."
