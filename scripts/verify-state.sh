32722bca3de5867210bdcb6f57c08622b5c269a9
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-08 13:02:18 -0500

    fix: improve error handling/output in verify-state.sh

--- scripts/verify-state.sh
+++ scripts/verify-state.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Check that user has permission to push to the repository.
-COLLABORATORS_URL="$(${GITHUB_ACTION_PATH}/scripts/github-event.sh .repository.collaborators_url)"
+COLLABORATORS_URL="$("${GITHUB_ACTION_PATH}"/scripts/github-event.sh .repository.collaborators_url)"
 
 # Build URL used to check for permissions
 COLLABORATORS_URL="${COLLABORATORS_URL}%\{/collaborator\}"
@@ -14,10 +14,10 @@ curl --silent --show-error -o "${PERM}" --location --globoff \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "X-GitHub-Api-Version: 2026-03-10" \
-  "${COLLABORATORS_URL}/$(${GITHUB_ACTION_PATH}/scripts/github-event.sh .sender.login)/permission"
+  "${COLLABORATORS_URL}/$("${GITHUB_ACTION_PATH}"/scripts/github-event.sh .sender.login)/permission"
 
 # Output push permissions state
-printf "HAS_PERMS=%s\n" "$(jq -r .user.permissions.push < ${PERM})" >> "${GITHUB_ENV}"
+printf "HAS_PERMS=%s\n" "$(jq -r .user.permissions.push < "${PERM}")" >> "${GITHUB_ENV}"
 
 # Verify if fast forwarding is possible
 # Create a local branch reference for the PR
@@ -26,3 +26,6 @@ git branch -f "pull_request/${HEAD_REF}" "${HEAD_SHA}"
 # Check if the base branch is a direct ancestor of the head branch
 IS_POSSIBLE=$(git merge-base --is-ancestor "${BASE_REF}" "${HEAD_SHA}" && echo true || echo false)
 printf "IS_POSSIBLE=%s\n" "${IS_POSSIBLE}" >> "${GITHUB_ENV}"
+
+printf "User has permission to push to target branch: %s\n" "${HAS_PERMS}" >> "${GITHUB_STEP_SUMMARY}"
+printf "Fast forwarding is possible: %s\n" "${IS_POSSIBLE}" >> "${GITHUB_STEP_SUMMARY}"
